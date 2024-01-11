31124d0f3e102f6a8981fc978a80ead69a9dd900
Houcem Eddine Mabrouki <houcemeddine.mabrouki@etudiant-isi.utm.tn>
2024-01-11 14:42:19 +0100

    added just a comment to test pipeline

--- backend/app.js
+++ backend/app.js
@@ -21,6 +21,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // Définition du modèle de contact
+//Commentaire pour le push du code
 const ContactSchema = new mongoose.Schema({
   name: String,
   lastName: String,
